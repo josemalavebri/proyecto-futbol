bf25f489b55cb6b6700a4525fd9aa7ebb62cfca2
MarcFalconV <mfalconivillamar10@outlook.com>
2024-12-15 13:05:53 -0500

    Subiendo componente Equipo y ListaEquipo

---
+++ src/app/components/lista-equipo/lista-equipo.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ListaEquipoComponent } from './lista-equipo.component';
+
+describe('ListaEquipoComponent', () => {
+  let component: ListaEquipoComponent;
+  let fixture: ComponentFixture<ListaEquipoComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ListaEquipoComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ListaEquipoComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
