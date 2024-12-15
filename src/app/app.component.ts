71849c6ba7cec2969992b617d1db5c20090e6c32
Jose <165485943+josemalavebri@users.noreply.github.com>
2024-12-15 13:10:59 -0500

    arreglando navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./componentes/navbar/navbar.component";
+import { PosicionesComponent } from "./componentes/posiciones/posiciones.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, PosicionesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { PosicionesComponent } from './componentes/posiciones/posiciones.component';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'posiciones', component: PosicionesComponent },
+    {path:'',component:PosicionesComponent},
+    { path: '**', redirectTo: '', pathMatch: 'full'}
+];
